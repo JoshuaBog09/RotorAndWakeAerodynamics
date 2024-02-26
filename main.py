df1acc8162fbe88e4132f1ca37c778eadc62d6ab
roelvanleeuwen <94065912+roelvanleeuwen@users.noreply.github.com>
2024-02-26 13:56:35 +0100

    Excel data reader

--- main.py
+++ main.py
@@ -1,4 +1,18 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import pandas as pd
 
 import design
+
+
+file_path = 'polar_DU95W180.xlsx'
+
+# Read the Excel file into a pandas DataFrame
+df = pd.read_excel(file_path)
+
+# Extract columns into NumPy arrays
+alfa_array = df['Alfa'].to_numpy()
+cl_array = df['Cl'].to_numpy()
+cd_array = df['Cd'].to_numpy()
+cm_array = df['Cm'].to_numpy()
+print()
