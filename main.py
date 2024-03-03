c837d8767f028eb293e2e6d9391685c5c9403c69
JoshuaBog09 <95229035+JoshuaBog09@users.noreply.github.com>
2024-03-03 12:05:30 +0100

    Devision into segment

--- main.py
+++ main.py
@@ -2,6 +2,9 @@ import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 
+import math
+import statistics
+
 import design
 
 
@@ -15,4 +18,38 @@ alfa_array = df['Alfa'].to_numpy()
 cl_array = df['Cl'].to_numpy()
 cd_array = df['Cd'].to_numpy()
 cm_array = df['Cm'].to_numpy()
-print()
+
+# Data over alpha plots
+
+plt.plot(alfa_array, cl_array, label="cl")
+plt.plot(alfa_array, cd_array, label="cd")
+plt.xlabel("Angle of attack [deg]")
+plt.ylabel("Coefficient [-]")
+plt.legend()
+plt.show()
+
+
+plt.plot(cd_array, cl_array, label="cd - cl")
+plt.xlabel("cd [-]")
+plt.ylabel("cl [-]")
+plt.legend()
+plt.show()
+
+## Define blade elemenets 
+
+resolution = 1000
+r_R = np.linspace(design.start, design.end, resolution,endpoint=True)
+
+# Loop over all segments and take mean conditions
+for i in range(len(r_R) - 1):
+
+    segment_start   = r_R[i]
+    segment_end     = r_R[i + 1]
+
+    segment_mean = statistics.mean([segment_start, segment_end])
+
+    segment_chord = design.chord(segment_mean)
+    segment_twist = design.twist(segment_mean)
+
+    # For each segment solve the Blade element momentum theory model
+
